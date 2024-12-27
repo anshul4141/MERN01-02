5917ebc7011c7ee164c1a2637fca6efa6d73af06
anshul4141 <anshulprajapati691@gmail.com>
2024-12-27 13:35:29 +0530

    done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,48 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Welcome from './component/Welcome';
 import SignUp from './component/SignUp';
 import Login from './component/Login';
-
-const Navigation = () => {
-  const user = JSON.parse(localStorage.getItem('user'));
-  console.log('user ===> ', user);
-  const navigate = useNavigate();
-
-  const logout = () => {
-    localStorage.removeItem('user');
-    navigate('/login');
-  };
-
-  return (
-    <nav>
-      {user ? (
-        <>
-          <h3>&nbsp; Hi, {user.user.firstName}</h3>
-          <Link to="/">Welcome</Link> |&nbsp;
-          <Link to="#" onClick={logout}>
-            Logout
-          </Link>
-        </>
-      ) : (
-        <>
-          <h3>&nbsp; Hi, guest</h3>
-          <Link to="/">Welcome</Link> |&nbsp;
-          <Link to="/login">Login</Link> |&nbsp;
-          <Link to="/signup">SignUp</Link>
-        </>
-      )}
-      <br></br>
-      <hr></hr>
-    </nav>
-  );
-};
 
 const App = () => {
   return (
     <Router>
       <div>
-        <Navigation />
+        <nav>
+          <br></br>
+          &nbsp;&nbsp;
+          <Link to="/">Welcome</Link> |&nbsp;
+          <Link to="/login">Login</Link> |&nbsp;
+          <Link to="/signup">SignUp</Link>
+          <br></br>
+          <hr></hr>
+        </nav>
+
         <Routes>
           <Route path="/" element={<Welcome />} />
           <Route path="/login" element={<Login />} />
@@ -51,6 +26,6 @@ const App = () => {
       </div>
     </Router>
   );
-};
+}
 
 export default App;
--- frontend/src/component/SignUp.js
+++ frontend/src/component/SignUp.js
@@ -9,95 +9,67 @@ const SignUp = () => {
     password: '',
     dob: '',
     gender: '',
-    address: '',
     role: 'user'
   });
-
-  const [message, setMessage] = useState('');
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
-    console.log('firstName ==>', formData.firstName);
-    console.log('lastName ==>', formData.lastName);
-    console.log('loginId ==>', formData.loginId);
+    console.log('firstName===>', formData.firstName);
+    console.log('lastName===>', formData.lastName);
+    console.log('logindId===>', formData.loginId);
   };
 
   const SignUp = (e) => {
+
     e.preventDefault();
     axios.post('http://localhost:5000/user/signUp', formData)
       .then((response) => {
-        setMessage('User registered successfully!');
-        console.log(response.data);
+        console.log('res ====>', response.data);
       })
       .catch((error) => {
-        setMessage('An error occurred.');
-        console.error(error);
-      });
-  };
+        console.log('error: ', error.message);
+      })
+
+  }
 
   return (
     <div>
-      <h1 align="center">User Registration</h1>
+      <h1 align='center'>User Registration</h1>
       <form onSubmit={SignUp}>
-        {message && (
-          <div align="center" style={{ marginTop: '20px', color: message.includes('successfully') ? 'green' : 'red' }}>
-            {message}
-          </div>
-        )}
-        <table align="center">
+        <table align='center'>
           <tr>
             <th>FirstName:</th>
-            <td>
-              <input type="text" name="firstName" value={formData.firstName} onChange={handleChange} placeholder="Enter Your FirstName" />
-            </td>
+            <td><input type='text' name='firstName' value={formData.firstName} onChange={handleChange} placeholder='Enter First Name' /></td>
           </tr>
           <tr>
             <th>LastName:</th>
-            <td>
-              <input type="text" name="lastName" value={formData.lastName} onChange={handleChange} placeholder="Enter Your LastName" />
-            </td>
+            <td><input type='text' name='lastName' value={formData.lastName} onChange={handleChange} placeholder='Enter Last Name' /></td>
           </tr>
           <tr>
             <th>LoginId:</th>
-            <td>
-              <input type="email" name="loginId" value={formData.loginId} onChange={handleChange} placeholder="Enter Your EmailId" />
-            </td>
+            <td><input type='email' name='loginId' value={formData.loginId} onChange={handleChange} placeholder='Enter EmailId' /></td>
           </tr>
           <tr>
             <th>Password:</th>
-            <td>
-              <input type="password" name="password" value={formData.password} onChange={handleChange} placeholder="Enter Your Password" />
-            </td>
+            <td><input type='password' name='password' value={formData.password} onChange={handleChange} placeholder='Enter Password' /></td>
           </tr>
           <tr>
             <th>DOB:</th>
-            <td>
-              <input type="date" name="dob" value={formData.dob} onChange={handleChange} placeholder="Enter Your Dob" />
-            </td>
+            <td><input type='date' name='dob' value={formData.dob} onChange={handleChange} /></td>
           </tr>
           <tr>
             <th>Gender:</th>
-            <td>
-              <input type="text" name="gender" value={formData.gender} onChange={handleChange} placeholder="Enter Your Gender" />
-            </td>
-          </tr>
-          <tr>
-            <th>Address:</th>
-            <td>
-              <input type="text" name="address" value={formData.address} onChange={handleChange} placeholder="Enter Your Address" />
-            </td>
+            <td><input type='text' name='gender' value={formData.gender} onChange={handleChange} placeholder='Enter Your Gender' /></td>
           </tr>
           <tr>
             <th></th>
-            <td>
-              <input type="submit" value="SignUp" />
-            </td>
+            <td><input type='submit' value='SignUp' /></td>
           </tr>
         </table>
       </form>
     </div>
-  );
-};
+  )
+}
 
-export default SignUp;
+export default SignUp
