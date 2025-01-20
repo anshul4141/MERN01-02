7cb421e1e54291fa85483ae91201c27b26330a7d
anshul4141 <anshulprajapati691@gmail.com>
2025-01-20 13:12:09 +0530

    done

---
+++ Ecome01-02/frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from './components/Layouts/Layout'
+
+const App = () => {
+  return (
+    <>
+      <Layout>
+        <h1>Ecommerce App</h1>
+      </Layout>
+    </>
+  )
+}
+
+export default App
+
